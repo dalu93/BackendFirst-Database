18e8607498598a8fbc9c9c973cb8856009fc1ee8
Luca D'Alberti <dalberti.luca93@gmail.com>
2016-03-25 14:18:55 +0100

    User table

---
+++ tables/users.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE users (
+
+	id serial primary key,
+	email text UNIQUE,
+	password TEXT NOT NULL
+);
+
+CREATE TABLE emails (
+
+	id serial primary key,
+	email text
+);
+
+create unique index on emails ((lower (email)));
